3430d095905805a39148021d38645fea5bf89ec9
Eugene Komisarenko <ekomisarenko@gmail.com>
2023-10-09 13:13:16 -0400

    fixed initial code

--- embedded/sensors/temperature/src/main.rs
+++ embedded/sensors/temperature/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use embedded_nal::TcpClientStack;
 use panic_halt as _;
 use arduino_hal::spi;
 use embedded_nal::{IpAddr, Ipv4Addr, SocketAddr};
@@ -9,31 +10,32 @@ use embedded_nal::{IpAddr, Ipv4Addr, SocketAddr};
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let select = pins.d10.into_output();
-
+    
     let (spi, _) = arduino_hal::Spi::new(
         dp.SPI,
         pins.d13.into_output(),
         pins.d11.into_output(),
         pins.d12.into_pull_up_input(),
-        select,
+        pins.d10.into_output(),
         spi::Settings::default(),
     );
 
-    let mut device = w5500::UninitializedDevice::new(w5500::bus::FourWire::new(spi, select))
+    let mut device = w5500::UninitializedDevice::new(w5500::bus::FourWire::new(spi, pins.d10.into_output()))
         .initialize_manual(
             w5500::MacAddress::new(0, 1, 2, 3, 4, 5),
-            w5500::net::Ipv4Addr::new(192, 168, 0, 190),
+            Ipv4Addr::new(192, 168, 0, 190),
             w5500::Mode::default()
         ).unwrap();
 
     let mut socket = device.socket().unwrap();
 
-    device.connect(&mut socket, SocketAddr::new(IpAddr::V4(w5500::net::IpAddr::new(192, 168, 0, 158)), 7632)).unwrap();
+    device.connect(&mut socket, SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 0, 158)), 7632)).unwrap();
 
     nb::block!(device.send(&mut socket, &[104, 101, 108, 108, 111, 10]));
 
     device.close(socket);
 
-    let (spi_bus, inactive_device) = device.deactivate();
+    let (_spi_bus, _inactive_device) = device.deactivate();
+
+    panic!();
 }
